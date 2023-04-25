b60df96a29befb587b43d2d15252501c8424b486
Amina Cherni <cherniamina21@gmail.com>
2023-04-25 16:05:40 +0200

    to uri

--- src/main/java/com/example/fines_saveurs/dao/DataBase.java
+++ src/main/java/com/example/fines_saveurs/dao/DataBase.java
@@ -3,6 +3,7 @@ package com.example.fines_saveurs.dao;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,7 +15,7 @@ public abstract class DataBase {
     public static Connection getConnection() {
         if (INSTANCE == null) {
             try {
-                String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+                String rootPath = Thread.currentThread().getContextClassLoader().getResource("").toURI().getPath();
                 String appConfigPath = rootPath + "app.properties";
                 Properties appProps = new Properties();
                 appProps.load(new FileInputStream(appConfigPath));
@@ -23,6 +24,8 @@ public abstract class DataBase {
                 Class.forName("com.mysql.cj.jdbc.Driver");
                 INSTANCE = DriverManager.getConnection(appProps.getProperty("bdd_url"), appProps.getProperty("bdd_user"), appProps.getProperty("bdd_password"));
             } catch (SQLException | ClassNotFoundException | IOException e) {
+                throw new RuntimeException(e);
+            } catch (URISyntaxException e) {
                 throw new RuntimeException(e);
             }
         }
