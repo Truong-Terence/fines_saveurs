b8459a1da800e14552ec483a9038c0d840ba3e36
Marie Chanaron <chanaronmarie@gmail.com>
2023-05-13 00:30:24 +0200

    check if there is any product in the category and if yes, display a trash to erase the category - Add edit icon in categories

--- src/main/java/com/example/fines_saveurs/service/CategoryService.java
+++ src/main/java/com/example/fines_saveurs/service/CategoryService.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class CategoryService {
+
     private final CategoryJdbcDao categoryDao = new CategoryJdbcDao();
 
     public boolean addCategory(String name) {
@@ -15,14 +16,17 @@ public class CategoryService {
         return categoryDao.create(newCategory);
     }
 
+    public List<Category> fetchEmptyCategories() {
+        return categoryDao.fetchEmptyCategories();
+    }
 
     public List<Category> fetchAllCategories() {
         return categoryDao.findAll();
     }
+
     public Category getById(Integer id) { return categoryDao.findById(id); }
 
     public boolean deleteCategory(int id) {
-
         return categoryDao.delete(new Category(id));
     }
 }
